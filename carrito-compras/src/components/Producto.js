f533dc70353d0124ba59569b6cae0d095106bf31
Santiago Sanmartín Quintero <santiago.sanmartin@ceiba.com.co>
2022-02-02 10:18:37 -0500

    Clase 43

--- carrito-compras/src/components/Producto.js
+++ carrito-compras/src/components/Producto.js
@@ -1,18 +1,23 @@
 import { Component } from "react";
 
 const styles = {
-    productos: {
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between'
+    producto: {
+        border: 'solid 1px #eee',
+        boxShadow: '0 5px 5px rgb(0,0,0,0.1)',
+        width: '30%',
+        padding: '10px 15px',
+        borderRadius: '5px'
+    },
+    img: {
+        width: '100%'
     }
 }
 class Producto extends Component {
     render(){
         const { producto } = this.props;
         return(
-            <div style={styles.productos}>
-                <img alt={producto.name} src = {producto.img} />
+            <div style={styles.producto}>
+                <img style={styles.img} alt={producto.name} src = {producto.img} />
                 <h3>{producto.name}</h3>
                 <p>{producto.price}</p>
             </div>
