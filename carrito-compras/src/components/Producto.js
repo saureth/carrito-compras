31756c4bdc44d0efc7fc7fa6e4801a19e312dd6b
Santiago Sanmartín <saureth001@gmail.com>
2022-02-02 10:37:44 -0500

    Clase 44

--- carrito-compras/src/components/Producto.js
+++ carrito-compras/src/components/Producto.js
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import Button from './Button'
 
 const styles = {
     producto: {
@@ -14,12 +15,15 @@ const styles = {
 }
 class Producto extends Component {
     render(){
-        const { producto } = this.props;
+        const { producto, agregarAlCarro } = this.props;
         return(
             <div style={styles.producto}>
                 <img style={styles.img} alt={producto.name} src = {producto.img} />
                 <h3>{producto.name}</h3>
                 <p>{producto.price}</p>
+                <Button onClick={() => agregarAlCarro(producto)}> 
+                    Agregar al carro
+                </Button>
             </div>
         );
     }
