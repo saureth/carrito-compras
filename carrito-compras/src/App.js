ad76cadb54d1d3d5fee88ffb7e35b5bc42995acc
Santiago Sanmartín <saureth001@gmail.com>
2022-02-02 12:22:08 -0500

    Clase 51

--- carrito-compras/src/App.js
+++ carrito-compras/src/App.js
@@ -16,16 +16,30 @@ class App extends Component {
       {
         name:'Lechuga', price: 500, img: '/productos/lechuga.jpg'
       }
-    ]
+    ],
+    carro: [
+      
+    ],
   }
+
+  agregarAlCarro = (producto) => {
+    return this.setState({
+      carro: this.state.carro.concat({
+        ...producto,
+        cantidad: 1,
+      })
+    });
+  }
+
   render(){
+    console.log(this.state.carro);
     return(
       <div>
         <Navbar />
         <Layout>
           <Title/>
           <Productos
-            agregarAlCarro={()=>console.log("Agregando al carrito")}
+            agregarAlCarro={this.agregarAlCarro}
             productos = {this.state.productos}
           />
         </Layout>
