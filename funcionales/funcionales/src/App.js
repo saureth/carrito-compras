71ebdce20320837588e4713bd51f08e7a065bd3c
Santiago Sanmartín <saureth001@gmail.com>
2022-02-03 08:50:42 -0500

    Clase 58

--- funcionales/funcionales/src/App.js
+++ funcionales/funcionales/src/App.js
@@ -1,9 +1,25 @@
-import {useState} from 'react';
+import {useState, Component} from 'react';
 
 // reglas: 
 // hooks no se llaman en loops, siempre al inicio del componente y al nivel más alto de la función
 // solo se llaman en componentes de react o custom hooks
 
+class App extends Component {
+    state = { contador: 0}
+
+    incrementar = () => {
+        this.setState({ contador: this.state.contador + 1})
+    }
+    render() {
+        return (
+            <div>
+                contador: {this.state.contador}
+                <button onClick={this.incrementar}> Incrementar </button>
+            </div>
+        );
+    }
+}
+/*
 const App = () => {
     const [contador, setContador] = useState(0);
     return (
@@ -12,6 +28,6 @@ const App = () => {
             <button onClick={()=>setContador(contador + 1)}> Incrementar </button>
         </div>
     );
-}
+}*/
 
 export default App;
