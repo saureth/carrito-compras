a78397758b2b8a480ba867d16091b603af48bed6
Santiago Sanmartín <saureth001@gmail.com>
2022-02-03 09:14:02 -0500

    Clase 61

--- funcionales/funcionales/src/App.js
+++ funcionales/funcionales/src/App.js
@@ -8,6 +8,16 @@ const useContador = (inicial) => {
     return [contador, incrementar];
 }
 
+const Interval = ({ contador }) => {
+    useEffect( () => {
+        const i = setInterval(()=> console.log(contador), 1000);
+        return () => clearInterval(i);
+    }, [contador]);
+    return (
+        <p> Intervalo </p>
+    );
+}
+
 const App = () => {
     const [contador, incrementar] = useContador(0);
     useEffect(()=>{
@@ -17,6 +27,7 @@ const App = () => {
         <div>
             Contador: {contador}
             <button onClick={incrementar}> Incrementar </button>
+            <Interval contador={contador}/>
         </div>
     );
 }
