cf026832fca747427693e3061b3b6a0a31a42f20
Santiago Sanmartín <saureth001@gmail.com>
2022-02-04 13:47:36 -0500

    Clase 78

--- formularios-practica/src/App.js
+++ formularios-practica/src/App.js
@@ -1,24 +1,29 @@
 import useFormulario from './hooks/useFormulario';
 import Input from './components/Input';
 import Card from './components/Card'
+import Container from './components/Container';
 
 function App() {
   const [formulario, handleChange] = useFormulario({ name: '', lastName: '' });
   return (
-    <Card>
-      <form>
-        <Input
-          label='Nombre'
-          name='name'
-          value={formulario.name}
-          onChange={handleChange} />
-        <Input
-          label='Apellido'
-          name='lastName'
-          value={formulario.lastName}
-          onChange={handleChange} />
-      </form>
-    </Card>
+    <Container>
+      <Card>
+        <div style={{padding:20}}>
+          <form>
+            <Input
+              label='Nombre'
+              name='name'
+              value={formulario.name}
+              onChange={handleChange} />
+            <Input
+              label='Apellido'
+              name='lastName'
+              value={formulario.lastName}
+              onChange={handleChange} />
+          </form>
+        </div>
+      </Card>
+    </Container>
   );
 }
 
