84fc6523345f88b3b2023851d0bf831856fa3ed1
Santiago Sanmartín <saureth001@gmail.com>
2022-02-04 12:03:51 -0500

    Clase 71

--- forms/src/App.js
+++ forms/src/App.js
@@ -2,7 +2,7 @@ import { useState } from "react"
 
 const App = () => {
 
-    const [value, setValue] = useState({ normal: 'por defecto', texto: ''});
+    const [value, setValue] = useState({ normal: 'por defecto', texto: '', select: ''});
 
     const handleChange = (e) => {
         setValue( (state) => ({
@@ -16,6 +16,15 @@ const App = () => {
             {value.length < 5 ? <span> longitud mínima de 5 </span> : null}
             <input type='text' name='normal' value={value.normal} onChange={handleChange} />
             <textarea name='texto' onChange={handleChange} value={value.texto} />
+
+            <select value={value.select} name='select' onChange={handleChange}>
+                <option value=''> -- Seleccione </option>
+                <option value='chanchofeliz'> Chancho feliz </option>
+                <option value='chanchitofeliz'> Chanchito feliz </option>
+                <option value='chanchitotriste'> Chanchito triste </option>
+                <option value='felipe'> Felipe </option>
+            </select>
+
         </div>
     );
 }
