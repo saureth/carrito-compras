aec3650c131a4f97d9b2975eab26fe00cc72a7bb
Santiago Sanmartín <saureth001@gmail.com>
2022-02-14 09:13:46 -0500

    Clase 104

--- styled/src/App.js
+++ styled/src/App.js
@@ -8,10 +8,20 @@ const Content = styled.div`
   padding: 20px 25px;
 `
 
+const Button = styled.button`
+  background-color: ${props => props.primary ? 'red':'white'};
+  color: ${props => props.primary ? 'white':'red'};
+  padding: 10px 15px;
+  border: solid 2px red;
+  border-radius: 4px;
+`
+
 function App() {
   return (
     <Content>
       <P>Soy un párrafo</P>
+      <Button> Enviar </Button>
+      <Button primary> Enviar </Button>
     </Content>
   );
 }
