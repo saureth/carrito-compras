a1f2f2c27f8c2765b55464f167663c43756651dd
Santiago Sanmartín <saureth001@gmail.com>
2022-02-14 09:42:02 -0500

    Clase 110

--- styled/src/App.js
+++ styled/src/App.js
@@ -3,10 +3,10 @@ import styled from 'styled-components';
 const P = styled.p`
   font-size: 24px;
   color: red;
-`
+`;
 const Content = styled.div`
   padding: 20px 25px;
-`
+`;
 
 const Button = styled.button`
   transition: box-shadow 0.2s ease;
@@ -29,18 +29,33 @@ const Button = styled.button`
   .info {
     font-size: 28px;
   }
-`
+`;
 const BlockButton = styled(Button)`
   width: 100%;
   font-size: 24px;
-`
+`;
 const Link = ({className, ...props}) => {
   return <a className={className} {...props} ></a>
 }
 
 const StyledLink = styled(Link)`
   color: blue;
-`
+`;
+
+const Input = styled.input.attrs(props => ({
+  type: 'text',
+  color: props.color || 'red'
+}))`
+  font-size: 20px;
+  border: 1px solid red;
+  color: ${props => props.color}
+`;
+
+const Password = styled(Input).attrs({
+  type: 'password'
+})`
+
+`;
 
 function App() {
   return (
@@ -55,6 +70,8 @@ function App() {
       <BlockButton primary > Enviar </BlockButton>
       <Link>Link</Link>
       <StyledLink> Link con estilo</StyledLink>
+      <Input color='blue'/>
+      <Password />
     </Content>
   );
 }
