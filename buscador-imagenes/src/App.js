9a69899cd759ac35a612a2b22f7376f28b265a75
Santiago Sanmartín <saureth001@gmail.com>
2022-02-14 07:50:34 -0500

    Clase 98

--- buscador-imagenes/src/App.js
+++ buscador-imagenes/src/App.js
@@ -1,6 +1,10 @@
 import {Formik, Form, Field} from 'formik';
-import './css/header.css'
+import { useState } from 'react';
+import './css/header.css';
+
 const App = () => {
+  const [photos, setPhotos] = useState([]);
+  console.log({photos});
   return (
     <div>
       <header>
@@ -8,7 +12,13 @@ const App = () => {
           initialValues={{search:''}}
           onSubmit={async values => {
             //llamado api
-            console.log(values)
+            const response = await fetch(`https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`,{
+              headers: {
+                'Authorization': 'Client-ID YLeNTIjnPO6z3_WR9nMYmkiS4lzota9JknRQoC9lM6E'
+              }
+            });
+            const data = await response.json();
+            setPhotos(data.results);
           }
         }>
           <Form>
