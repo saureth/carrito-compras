9544298d11ea8cf4f1134ea9f651b67c8cc4977e
Santiago Sanmartín <saureth001@gmail.com>
2022-02-14 09:05:30 -0500

    Clase 102

---
+++ styled/src/App.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const P = styled.p`
+  font-size: 24px;
+  color: red;
+`
+function App() {
+  return (
+    <div>
+      <P>Soy un párrafo</P>
+    </div>
+  );
+}
+
+export default App;
