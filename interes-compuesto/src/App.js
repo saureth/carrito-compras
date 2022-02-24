6c90b9a777c296f04c1772238dc65b21cd0b8922
Santiago Sanmartín <saureth001@gmail.com>
2022-02-24 09:27:01 -0500

    Clase 115

--- interes-compuesto/src/App.js
+++ interes-compuesto/src/App.js
@@ -6,9 +6,22 @@ const Container = styled.div`
   height: 100%;
   align-items: center;
 `
+
+const Section = styled.section`
+  background-color: #eee;
+  border-top: solid 2px palevioletred;
+  padding: 20px 25px;
+  width: 500px;
+  box-shadow: 0px 2px 3px rgb(0,0,0,0.3);
+`
+
 function App() {
   return (
-    <Container> lala </Container>
+    <Container> 
+      <Section>
+        lala
+      </Section>
+    </Container>
   );
 }
 
