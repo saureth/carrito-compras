0447541eb6030f09802798e7607ece21a5f830bd
Santiago Sanmartín <saureth001@gmail.com>
2022-02-24 09:55:53 -0500

    Clase 117

--- interes-compuesto/src/App.js
+++ interes-compuesto/src/App.js
@@ -1,25 +1,36 @@
 import styled from 'styled-components';
-
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  height: 100%;
-  align-items: center;
-`
-
-const Section = styled.section`
-  background-color: #eee;
-  border-top: solid 2px palevioletred;
-  padding: 20px 25px;
-  width: 500px;
-  box-shadow: 0px 2px 3px rgb(0,0,0,0.3);
-`
+import {Formik, Form } from 'formik';
+import Input from './components/Input';
+import Button from './components/Button';
+import Section from './components/Section';
+import Container from './components/Container';
 
 function App() {
+
+  const handleSubmit = () => {
+    
+  }
+
   return (
     <Container> 
       <Section>
-        lala
+        <Formik
+          initialValues={{
+            deposit: '',
+            contribution: '',
+            years: '',
+            rate: ''
+          }}
+          onSubmit={handleSubmit}
+          >
+            <Form>
+              <Input name='deposit' label='Depósito inicial' />
+              <Input name='contribution' label='Contribución anual' />
+              <Input name='years' label='Años' />
+              <Input name='rate' label='Interés estimado' />
+              <Button>Calcular</Button>
+            </Form>            
+        </Formik>
       </Section>
     </Container>
   );
