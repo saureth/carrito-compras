67f1d51dc01dba589f60e1e40ee58cd793d8744b
Santiago Sanmartín <saureth001@gmail.com>
2022-08-02 02:19:50 -0500

    Clase 138

--- router/src/App.js
+++ router/src/App.js
@@ -1,5 +1,40 @@
 import { Routes, Route, Link } from 'react-router-dom';
 
+const Home = () => {
+    return (
+        <h1>Inicio</h1>
+    );
+}
+
+const Portafolio = () => {
+    return (
+        <div>
+            <h1>Portafolio</h1>
+            <ul>
+                <li>
+                    <Link to={"/portafolio/proyecto-1"}>Proyecto 1</Link>
+                </li>
+                <li>
+                    <Link to={"/portafolio/proyecto-2"}>Proyecto 2</Link>
+                </li>
+            </ul>
+            <div>
+                <Routes>
+                    <Route path={"/portafolio/proyecto-1"} element = {<Proyecto1 />}> </Route>  
+                    <Route path={"/portafolio/proyecto-2"} element ={<Proyecto2 />}> </Route>  
+                </Routes>
+            </div>
+        </div>        
+    );
+}
+
+const Proyecto1 = () => {
+    <h2>Proyecto 1</h2>
+}
+const Proyecto2 = () => {
+    <h2>Proyecto 2</h2>
+}
+
 function App() {
   return (
     <div>
@@ -15,12 +50,8 @@ function App() {
       </nav>
       <section>
         <Routes>
-          <Route path="/portafolio">
-            <h1>Portafolio</h1>
-          </Route>  
-          <Route path="/">
-            <h1>Inicio</h1>
-          </Route>
+          <Route path="/portafolio/*" element={<Portafolio />}> </Route>  
+          <Route exact path="/" element={<Home />}> </Route>
         </Routes>        
       </section>
     </div>
