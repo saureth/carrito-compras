28beb926785d713b2f6b288950a4d37bc87fa92e
Santiago Sanmartín <saureth001@gmail.com>
2022-12-23 09:34:54 -0500

    Clase 170

---
+++ poke/pages/index.js
@@ -0,0 +1,26 @@
+const Pokemon = ({ pokemon}) => {
+  return <li>{pokemon.name}</li>
+}
+
+export default function Pokemones({ pokemones }) {
+   return (
+    <div>
+      <p>Pokemones</p>
+      <ul>
+        {pokemones.map(pokemon => 
+          <Pokemon 
+            pokemon = {pokemon}
+            key={pokemon.name}
+          />)}
+      </ul>
+    </div>
+  )
+}
+
+export const getStaticProps = async () => {
+  const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
+  const data = await response.json();
+  return {
+    props: {pokemones: data.results}
+  }
+}
